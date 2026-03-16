6f96b3b56b55743ae8022e331437a2c5aa397f14
kirito <nayangondhali999@gmail.com>
2026-03-16 09:58:54 +0530

    Fix lint of code

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return "<h1>Hello, Flask!</h1><p>This is your first web app.</p>"
 
+
 @app.route('/greet/<name>')
 def greet(name):
     return f"Hello, {name}! Nice to meet you."
+
 
 if __name__ == '__main__':
     app.run(debug=True)
